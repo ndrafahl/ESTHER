98a241e80700236932cb57ee9f63289511b61120
russellrook <russell.s.rook@gmail.com>
2017-02-20 20:40:03 -0600

    Implemented DAO for NN

---
+++ StarterV1b/DAOFiles/NeuralNetworkDAO.java
@@ -0,0 +1,164 @@
+package DAOFiles;
+
+import NeuralNetwork.NeuralNetworkBluePrint;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Russell on 2/17/2017.
+ *
+ * This is a Data Access Object meant for loading and saving neural network
+ * blue prints to a file so they can be saved and reused.
+ */
+public class NeuralNetworkDAO  {
+
+    /**********************************************************
+     * loadNeuralNetworkList takes in a file name and outputs an array of neural network blueprints.
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+
+    public ArrayList<NeuralNetworkBluePrint> loadNeuralNetworkList(String filePath) throws IOException{
+        ArrayList<NeuralNetworkBluePrint> neuralNetworkList = new ArrayList<>(169);
+        Scanner fileIn;
+        int x, y, numOfInputs, numOfHiddenLayers, numOfOutputs;
+        int[] numOfNeuronsPerLayer;
+        String[] temp;
+        double[] inputBias, outputBias;
+        double[][] hiddenLayerBias, outputWeights, inputWeights;
+        double[][][] hiddenLayerWeights;
+
+        try{
+            fileIn = new Scanner(new FileInputStream(filePath));
+            while(fileIn.hasNext()){            //This line checks if there is another neural network to be constructed from the file.
+                numOfInputs = Integer.valueOf(fileIn.nextLine());   //The first line of each neural network is the number of input neurons.
+                inputWeights = new double[numOfInputs][];           //Constructs a double array the size of the number of inputs. This stores an array of each neuron weights.
+                inputBias = new double[numOfInputs];                //Same as above but stores the bias for each input neuron.
+                for(x = 0; x < numOfInputs; x++){                   //This for-loop reads the input weights into an array of arrays.
+                    temp = fileIn.nextLine().split(",");
+                    inputWeights[x][0] = Double.valueOf(temp[x]);
+                }
+                temp = fileIn.nextLine().split(",");
+                for(x = 0; x < numOfInputs; x++){                   //This for-loop takes the string bias and turn them to doubles and places them in the input bias array.
+                    inputBias[x] = Double.valueOf(temp[x]);
+                }
+                numOfHiddenLayers = Integer.valueOf(fileIn.nextLine());   //This saves the number of hidden layers.
+                temp = fileIn.nextLine().split(",");
+                numOfNeuronsPerLayer = new int[numOfHiddenLayers];       //Number of neurons per hidden layer.
+                for (x = 0; x < numOfHiddenLayers; x++){
+                    numOfNeuronsPerLayer[x] = Integer.valueOf(temp[x]);
+                }
+                hiddenLayerWeights = new double[numOfHiddenLayers][][];
+                for(x = 0; x < numOfHiddenLayers; x++){                  //Reads in the weights for each neuron in each hidden layer.
+                    for(y = 0; y < numOfNeuronsPerLayer[x]; y++){
+                        temp = fileIn.nextLine().split(",");
+                        for (int z = 0; z < temp.length; z++) {
+                            hiddenLayerWeights[x][y][z] = Double.valueOf(temp[z]);
+                        }
+                    }
+                }
+                hiddenLayerBias = new double[numOfHiddenLayers][];
+                for(x = 0; x < numOfHiddenLayers; x++){                    //Reads in the bias for each neuron in each hidden layer.
+                    temp = fileIn.nextLine().split(",");
+                    for(y = 0; y < temp.length; y++){
+                        hiddenLayerBias[x][y] = Double.valueOf(temp[y]);
+                    }
+                }
+                numOfOutputs = Integer.valueOf(fileIn.nextLine());          //Output layer size.
+                outputWeights = new double[numOfOutputs][];
+                for(x = 0; x < numOfOutputs; x++){                         //Reads the weights for each neuron in the output layer.
+                    temp = fileIn.nextLine().split(",");
+                    for (y = 0; y < temp.length; y++){
+                        outputWeights[x][y] = Double.valueOf(temp[y]);
+                    }
+                }
+                outputBias = new double[numOfOutputs];
+                temp = fileIn.nextLine().split(",");
+                for(x = 0; x < numOfOutputs; x++){                         //Reads the bias for each neuron in the output layer.
+                    outputBias[x] = Double.valueOf(temp[x]);
+                }
+                neuralNetworkList.add(new NeuralNetworkBluePrint(numOfInputs, inputWeights, inputBias,
+                        numOfHiddenLayers, numOfNeuronsPerLayer, hiddenLayerWeights,
+                        hiddenLayerBias, numOfOutputs, outputWeights, outputBias));      //After all the information for that neural network has been read from the file it is used to construct a blueprint and placed in an array of blueprints.
+            }
+
+            fileIn.close();
+        }
+        catch (IOException e){
+            throw e;
+        }
+
+
+        return neuralNetworkList;
+    }
+
+    /*******************************************************************************
+     * saveNeuralNetworkList method will most likely only be used during the training function
+     * in order to save the best neural networks for a specific pair of cards to a file.
+     * This method appends the neural network blueprint to the end of the file so a full
+     * working player can created for later playing. This method can only be passed
+     * one blueprint at a time. It is not meant and nor should it be used to write an entire
+     * array of neural network blueprints.
+     *
+     * @param neuralNetwork
+     * @param filePath
+     * @throws IOException
+     ********************************************************************************/
+
+    public void saveNeuralNetworkList(NeuralNetwork.NeuralNetworkBluePrint neuralNetwork, String filePath) throws IOException{
+        FileWriter fileOut;
+        int i;
+
+        try{
+            fileOut = new FileWriter(filePath, true);     //This is a file writer that appends to the end of the file instead of overwriting it.
+            fileOut.write(String.valueOf(neuralNetwork.getNumOfInputs()) + "\r\n");               //The rest of the code just writes all the variables from the blueprint to its own line in the same way it is read above.
+            for(i = 0; i < neuralNetwork.getNumOfInputs(); i++){
+                fileOut.write(String.valueOf(neuralNetwork.getInputWeights()[i][0]) + "\r\n");
+            }
+            for(i = 0; i < neuralNetwork.getInputBias().length - 1; i++){
+                fileOut.write(String.valueOf(neuralNetwork.getInputBias()[i]) + ",");
+            }
+            fileOut.write(String.valueOf(neuralNetwork.getInputBias()[neuralNetwork.getInputBias().length - 1]) + "\r\n");
+            fileOut.write(String.valueOf(neuralNetwork.getNumOfHiddenLayers()) + "\r\n");
+            for(i = 0; i < neuralNetwork.getNumOfNeuronsPerLayer().length - 1; i++){
+                fileOut.write(String.valueOf(neuralNetwork.getNumOfNeuronsPerLayer()[i]) + ",");
+            }
+            fileOut.write(String.valueOf(neuralNetwork.getNumOfNeuronsPerLayer()[neuralNetwork.getNumOfNeuronsPerLayer().length - 1]) + "\r\n");
+            for(i = 0; i < neuralNetwork.getHiddenLayerWeights().length; i++){
+                for(int j = 0; j < neuralNetwork.getHiddenLayerWeights()[i].length; j++){
+                    for(int k = 0; k < neuralNetwork.getHiddenLayerWeights()[i][j].length - 1; k++){
+                        fileOut.write(String.valueOf(neuralNetwork.getHiddenLayerWeights()[i][j][k]) + ",");
+                    }
+                    fileOut.write(String.valueOf(neuralNetwork.getHiddenLayerWeights()[i][j][neuralNetwork.getHiddenLayerWeights()[i][j].length - 1]) + "\r\n");
+                }
+            }
+            for(i = 0; i < neuralNetwork.getHiddenLayerBias().length; i++){
+                for(int j = 0; j < neuralNetwork.getHiddenLayerBias()[i].length - 1; j++){
+                    fileOut.write(String.valueOf(neuralNetwork.getHiddenLayerBias()[i][j]) + ",");
+                }
+                fileOut.write(String.valueOf(neuralNetwork.getHiddenLayerBias()[i][neuralNetwork.getHiddenLayerBias()[i].length - 1]) + "\r\n");
+            }
+            fileOut.write(String.valueOf(neuralNetwork.getNumOfOutputs()) + "\r\n");
+            for(i = 0; i < neuralNetwork.getOutputWeights().length; i++){
+                for(int j = 0; j < neuralNetwork.getOutputWeights()[i].length - 1; j++){
+                    fileOut.write(String.valueOf(neuralNetwork.getOutputWeights()[i][j]) + ",");
+                }
+                fileOut.write(String.valueOf(neuralNetwork.getOutputWeights()[i][neuralNetwork.getHiddenLayerBias()[i].length - 1]) + "\r\n");
+            }
+            for(i = 0; i < neuralNetwork.getOutputBias().length - 1; i++){
+                fileOut.write(String.valueOf(neuralNetwork.getOutputBias()[i]) + ",");
+            }
+            fileOut.write(String.valueOf(neuralNetwork.getNumOfNeuronsPerLayer()[neuralNetwork.getOutputBias().length - 1]) + "\r\n");
+
+            fileOut.close();
+
+        }
+        catch (IOException e){
+            throw e;
+        }
+    }
+}
