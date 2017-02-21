2ae85637a39d9082c5784f1183c4e3a300118530
russellrook <russell.s.rook@gmail.com>
2017-02-20 20:41:11 -0600

    Almost complete NN, only minor alterations needed.

--- StarterV1b/NeuralNetwork/HiddenLayer.java
+++ StarterV1b/NeuralNetwork/HiddenLayer.java
@@ -1,16 +1,30 @@
 package NeuralNetwork;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Russell on 2/16/2017.
  */
 public class HiddenLayer extends NetworkLayer {
 
-    public HiddenLayer(int outputLayerSize, int inputLayerSize){
-        layerArray = new ArrayList<Neuron>(outputLayerSize);
-        for(x = 0; x < outputLayerSize; x++){
-            layerArray.add(x, new SigmoidNeuron(inputLayerSize));
+    NetworkLayer previousLayer;
+
+    public HiddenLayer(int size, double[][] weights, double[] bias, NetworkLayer aThePreviousLayer){
+        previousLayer = aThePreviousLayer;
+        layerSize = size;
+        outputArray = new double[size];
+        layerArray = new ArrayList<Neuron>(size);
+        for(int x = 0; x < size; x++){
+            layerArray.add(new SigmoidNeuron(weights[x], bias[x]));
+        }
+    }
+
+    public void computeOutputs(){
+        double[] inputs = previousLayer.getNeuronLayerOutputs();
+        for(int x = 0; x < layerSize; x++){
+            layerArray.get(x).computeOutput(inputs);
+            outputArray[x] = layerArray.get(x).getOutput();
         }
     }
 }
