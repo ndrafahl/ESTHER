d661e75227b8c174f73011b718ee6cac76b68252
Alex Kehrli <kehrlia@uni.edu>
2017-03-22 10:51:18 -0500

    Added new tree implementation. Works

---
+++ StarterV1b/TreeNode.java
@@ -0,0 +1,64 @@
+//import sun.reflect.generics.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Alex on 3/22/2017.
+ */
+
+
+public class TreeNode<T> { //} implements Iterable<TreeNode<T>> {
+
+    private T data;
+    private TreeNode<T> parent;
+    private List<TreeNode<T>> children;
+
+    public TreeNode(T data) {
+        this.data = data;
+        this.children = new LinkedList<TreeNode<T>>();
+    }
+
+    public void addChild(T child) {
+        TreeNode<T> childNode = new TreeNode<T>(child);
+        childNode.setParent(this);
+        this.children.add(childNode);
+        // return childNode;             //want to return child node?
+    }
+    public void addChild(TreeNode child){
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+    private void setParent(TreeNode<T> parent){
+        this.parent = parent;
+    }
+
+    public TreeNode<T> getParent(){
+        return this.parent;
+    }
+
+    public T getData(){
+        return this.data;
+    }
+
+    public List<TreeNode<T>> getChildren(){
+        return this.children;
+    }
+
+    public boolean hasChildren() { return !children.isEmpty();}
+    // other features ...
+
+    public void readChildren(TreeNode<T> node){
+        for (TreeNode child : node.getChildren()) {
+            System.out.println(child.getData());
+            if(child.hasChildren()){
+                System.out.println("Has grandchild!");
+                readChildren(child);
+            }
+        }
+
+    }
+
+}
+
